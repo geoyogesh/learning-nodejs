a7a86f06498b4a536c87d50b5abe1baebead9fcf
geoyogesh <geoyogesh@gmail.com>
2014-08-20 21:03:39 -0500

    challenge one completed

---
+++ 05-challenge1/app.js
@@ -0,0 +1,23 @@
+// Load module dependencies
+var http = require('http'),
+fs = require('fs');
+http.createServer(function(request, response) {
+// the ASYNCHRONOUS, NON-BLOCKING API for reading in a file.
+fs.readFile('./data.csv', 'utf-8', function(err, data) {
+var responseData = {};
+// Basic JS: Work with the data in the file, and create the response
+var lines = data.split('\n');
+// Note the native forEach support in Arrays in node.js!
+lines.forEach(function(line) {
+var parts = line.split(',');
+responseData[parts[0]] = parts[1];
+});
+// set the content type to json
+response.writeHead(200, {
+'Content-Type':'application/json'
+});
+//serialize responseData to a JSON string
+response.end(JSON.stringify(responseData));
+});
+}).listen(3000);
+console.log('node server running on port 3000');
